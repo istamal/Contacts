9d6d290b5d3cee7e57239ffb9fb89764dd568297
istamal <dagalaev@gmail.com>
2020-10-08 08:59:02 +0300

    feature: add abilityto edit contact

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -10,6 +10,7 @@ import {
   Card,
   Table,
   notification,
+  Modal,
 } from 'antd';
 
 const Wrapper = styled.div`
@@ -39,7 +40,8 @@ const tailLayout = {
 
 const Contacts = () => {
   const [contacts, setCotcontacts] = React.useState([]);
-  const [selected, setSelected] = React.useState(null);
+  const [selected, setSelected] = React.useState([[], []]);
+  const [visible, setVisible] = React.useState(false);
 
   const openNotificationWithIcon = (type, message, description) => {
     notification[type]({
@@ -48,10 +50,31 @@ const Contacts = () => {
     });
   };
 
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = (e) => {
+    setVisible(false);
+  };
+
+  const handleCancel = (e) => {
+    setVisible(false);
+  };
+
+  const handleEdit = (contact) => {
+    if (!contact[0].length) {
+      openNotificationWithIcon('error', 'Контакт не выбран', 'Пожалуйста выберите один из контактов');
+    } else if (contact[0].length > 1) {
+      openNotificationWithIcon('error', 'Так много?', 'Пожалуйста выберите только один из контактов');
+    } else {
+      showModal();
+    }
+  };
+
   const rowSelection = {
-    // eslint-disable-next-line no-unused-vars
     onChange: (selectedRowKeys, selectedRows) => {
-      setSelected(selectedRowKeys);
+      setSelected([selectedRowKeys, selectedRows]);
     },
     getCheckboxProps: (record) => ({
       disabled: record.name === 'Disabled User', // Column configuration not to be checked
@@ -69,11 +92,10 @@ const Contacts = () => {
     fetchContacts();
   }, []);
 
-  const handleDelete = async (row) => {
-    if (row) {
-      await row.forEach((id) => axios.delete(`http://localhost:3004/posts/${id}`));
+  const handleDelete = async (contact) => {
+    if (contact[0].length) {
+      await contact[0].forEach((id) => axios.delete(`http://localhost:3004/posts/${id}`));
       openNotificationWithIcon('success', 'Окей!', 'Контакт удолен');
-      setSelected(null);
     } else {
       openNotificationWithIcon('error', 'Контакт не выбран', 'Убедитесь что выбрали контакт');
     }
@@ -108,6 +130,14 @@ const Contacts = () => {
     setCotcontacts([...contacts, contact]);
   };
 
+  const saveEditedContact = async (values) => {
+    const response = await axios.put(`http://localhost:3004/posts/${selected[0]}`, values);
+    const { data } = response;
+    const newContacts = contacts.filter((el) => el.key !== data.id);
+    const editedContact = { key: data.id, name: data.name, number: data.number };
+    setCotcontacts([editedContact, ...newContacts]);
+  };
+
   const onFinishFailed = (errorInfo) => {
     // eslint-disable-next-line no-console
     console.log('Failed:', errorInfo);
@@ -115,6 +145,55 @@ const Contacts = () => {
 
   return (
     <Wrapper>
+      <Modal
+        title="Basic Modal"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{
+            number: selected[0].length && selected[1][0].number,
+            name: selected[0].length && selected[1][0].name,
+          }}
+          onFinish={saveEditedContact}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Номер"
+            name="number"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста введите номер',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Имя"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста введите имя',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Сохранить
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
       <Card className="set-contact" title="Добавить контакт" bordered={false} style={{ width: 300 }}>
         <Form
           {...layout}
@@ -153,7 +232,7 @@ const Contacts = () => {
 
           <Form.Item {...tailLayout}>
             <Button type="primary" htmlType="submit">
-              Submit
+              Отправить
             </Button>
           </Form.Item>
         </Form>
@@ -161,7 +240,7 @@ const Contacts = () => {
       <Card title="Контакты" bordered={false} style={{ width: 300 }}>
         <Input.Search onSearch={handleSeach} className="margin-bottom" placeholder="Введите название контакта" enterButton />
         <Button onClick={() => handleDelete(selected)} className="margin-right" type="primary" danger>Удолить</Button>
-        <Button className="margin-bottom" type="primary">Редактрировать</Button>
+        <Button onClick={() => handleEdit(selected)} className="margin-bottom" type="primary">Редактрировать</Button>
         <Table
           rowSelection={{
             ...rowSelection,
